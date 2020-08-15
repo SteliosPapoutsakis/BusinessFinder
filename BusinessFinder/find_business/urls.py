95abeb07f5f7c1c1b7c1a60b53715366bd37970d
Stelios Papoutsakis <stelios.papoutsakis4@gmail.com>
2020-08-15 15:42:32 -0700

    made sample json endpoint

--- BusinessFinder/find_business/urls.py
+++ BusinessFinder/find_business/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('business_query', views.business_query, name='business_query'),
+
 ]
--- BusinessFinder/find_business/views.py
+++ BusinessFinder/find_business/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the businessFinder index.")
 # Create your views here.
+    
+def business_query(request):
+    data = {"test": "hello"}
+    return JsonResponse(data)
+
