1a8f75bb64d1b319a386df938ad26e6897e131ce
Spencer Rose <spencer.h.rose@icloud.com>
2020-08-15 15:56:40 -0700

    Attempted to use index template for view

--- BusinessFinder/find_business/views.py
+++ BusinessFinder/find_business/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the businessFinder index.")
+    return render(request, 'find_business/index.html')
 # Create your views here.
