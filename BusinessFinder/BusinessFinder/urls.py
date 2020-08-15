e061021f92b3bdd1d6c34e1c552f6df57c653a40
Spencer Rose <spencer.h.rose@icloud.com>
2020-08-15 15:29:55 -0700

    Finished creation of page for find_business

--- BusinessFinder/BusinessFinder/urls.py
+++ BusinessFinder/BusinessFinder/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('find_business/', include('find_business.urls')),
     path('admin/', admin.site.urls),
 ]
