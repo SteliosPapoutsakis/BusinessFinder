292aca5188c14859ed228bb73877329f1533978f
Jason-Twigg <jasontwigg1@gmail.com>
2020-08-15 21:47:39 -0700

    Added prototype for query from database
    No queries

--- BusinessFinder/find_business/views.py
+++ BusinessFinder/find_business/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 
 from .models import CompanyTypes, Company
 
+COORDINATE_TO_MILES = 75.0
 
 def index(request):
     companies = Company.objects.all()
@@ -23,13 +24,38 @@ def business_query(request):
     if request.method == 'POST':
         print(request.POST)
         query_dict = {}
-        for company_attr in ('type','title','lat','lon','within'):
+        valid_dict = {}
+        for company_attr in ('type','search','lat','lon','within'):
             val = request.POST.get(company_attr)
+        
             if val:
+                valid_dict[val] = True
                 query_dict[company_attr] = val
+            else:
+                valid_dict[val] = False
 
-        companies = ""#Company.objects.filter(query_dict)
+        companies = Company.objects.filter(
+            Q(name__contains(query_dict['search'])) |
+            Q(description__contains(query_dict['search']))
+        )
+        if (valid_dict['type']):
+            companies = companies.filter(type=query_dict['type'])
 
-        return JsonResponse('')
+        if (valid_dict['within'] & query_dict['within'] > 0):
+            dist = query_dict['within']/COORDINATE_TO_MILES
+            lat_max = query_dict['lat'] + dist
+            lat_min = query_dict['lat'] - dist
+            lon_max = query_dict['lon'] + dist
+            lon_min = query_dict['lon'] - dist
+            companies = companies.filter(
+                (Q(coordinatesLat__lte=lat_max) |
+                Q(coordinatesLat__gte=lat_min)) &
+                (Q(coordinatesLat__lte=lon_max) |
+                Q(coordinateLat__gte=lon_min));
+            )
+
+        print companies
+
+        return ''
     else:
         raise Http404()
