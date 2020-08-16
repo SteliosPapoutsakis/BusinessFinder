3403be247275bf357ec8ca4af481abecb3923412
Jason-Twigg <jasontwigg1@gmail.com>
2020-08-15 20:18:07 -0700

    Modified database to store url and have default vals
    Setup admin route to allow easy entry and modification of data in the
    database.

--- BusinessFinder/find_business/admin.py
+++ BusinessFinder/find_business/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Company, Activity
 # Register your models here.
+
+admin.site.register(Company)
--- BusinessFinder/find_business/models.py
+++ BusinessFinder/find_business/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from enum import IntEnum
-
+import datetime
+#DROP DATABASE IF EXISTS `businessfinderdb`; CREATE DATABASE `businessfinderdb` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci; USE 'mysql'; GRANT ALL PRIVILEGES ON mydb.* TO 'webappuser'@'75.164.93.129' IDENTIFIED BY 'hack20up' WITH GRANT OPTION; FLUSH PRIVILEGES;
 
 class CompanyTypes(IntEnum):
     RESTAURANT = 1
@@ -12,29 +13,25 @@ class CompanyTypes(IntEnum):
     def choices(cls):
         return [(key.value, key.name) for key in cls]
 
+#c = Company(type=CompanyTypes.RECREATION, name='Chicken Shack', address='5000 N Willamette Blvd', ulr='google.com')
+
 # Create your models here.
 class Company(models.Model):
     type = models.IntegerField(choices=CompanyTypes.choices()
         , default=CompanyTypes.RESTAURANT)
-    name = models.CharField('Company Name', max_length=100)
-    address = models.CharField('Company Address', max_length=150)
- 
-    hours_start = models.IntegerField('Company Hours Start',
-        default=0,
-        validators=(
-            MinValueValidator(0), MaxValueValidator(24)
-        )
-    )
-    hours_end = models.PositiveIntegerField('Company Hours End',
-        default=0,
-        validators=(
-            MinValueValidator(0), MaxValueValidator(24)
-        )
-    )
-    coordinatesLon = models.FloatField('Coordinates Longitude')
-    coordinatesLat = models.FloatField('Coordinates Latitude')
-    description = models.TextField('Company Description', max_length=512)
-    rating = models.FloatField('Company Rating',
+    name = models.CharField(default='Invalid Name',
+        max_length=100)
+    address = models.CharField(default='123 N Ali Ave',
+        max_length=150)
+    url = models.TextField(default='google.com')
+    start_time = models.TimeField(
+        default=datetime.datetime(2020, 8, 30, 6, 00, 00))
+    end_time = models.TimeField(
+        default=datetime.datetime(2020, 8, 30, 17, 0, 0))
+    coordinatesLon = models.FloatField(default=0.0)
+    coordinatesLat = models.FloatField(default=0.0)
+    description = models.TextField(default='', max_length=512)
+    rating = models.FloatField(
         default=5.0,
         validators=(
             MinValueValidator(0.0), MaxValueValidator(5.0)
@@ -42,15 +39,15 @@ class Company(models.Model):
     )
     
     def __str__(self):
-        return "%s" % (self.name)
+        return self.name
 
     def get_company_type_label(self):
         return CompanyTypes(self.type).name.title()
 
 class Activity(models.Model):
-    title = models.CharField('Activity Title',
+    title = models.CharField(default='Invalid Name',
         max_length=100)
-    description = models.TextField('Activity Description',
+    description = models.TextField(default='',
         max_length=500)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
